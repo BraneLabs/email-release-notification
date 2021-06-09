a8089521955059e769acf35512a218d77006eb92
Daniel Naves de Carvalho <Daniel@wearebrane.com>
2021-06-09 16:10:02 -0300

    adding release regex

--- src/notify.js
+++ src/notify.js
@@ -13,6 +13,7 @@ async function prepareMessage(recipients, lists) {
   const repoURL = repository.html_url;
   const repoDescription = repository.description ? `, ${repository.description.charAt(0).toLowerCase()+repository.description.slice(1)}` : '';
   const releaseVersion = release.tag_name;
+  const releaseRegex = process.env.RELEASE_REGEX;
   const releaseName = release.name;
   const releaseURL = release.html_url;
   const ownerResponse = await axios.get(repository.owner.url);
@@ -26,28 +27,34 @@ async function prepareMessage(recipients, lists) {
 
   const sender = process.env.RELEASE_SENDER_EMAIL;
 
-  return {
-    from: sender,
-    to: lists,
-    bcc: recipients,
-    subject: subject,
-    html: releaseBody,
-  };
+   if (releaseRegex == null || new RegExp(releaseRegex).match(releaseVersion) != null) {
+    return {
+      from: sender,
+      to: lists,
+      bcc: recipients,
+      subject: subject,
+      html: releaseBody,
+    };
+   } else {
+     return false;
+   }
 }
 async function run(recipientsUrl, distributionLists) {
   const { data } = await axios.get(recipientsUrl);
   const recipients = data.split(/\r\n|\n|\r/);
   const lists = distributionLists ? distributionLists.split(',') : [];
   const message = await prepareMessage(recipients, lists);
-  await mailgun.messages().send(message, function (err, body) {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            console.log("Sent!")
-            console.log(body);
-        }
-    });
+  if(message != false) {
+    await mailgun.messages().send(message, function (err, body) {
+          if (err) {
+              console.error(err);
+          }
+          else {
+              console.log("Sent!")
+              console.log(body);
+          }
+      });
+  }
 }
 
 /**
